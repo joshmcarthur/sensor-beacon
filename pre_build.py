e66d79803e7aeb3072c604b402bfc16edd176508
Josh McArthur <joshua.mcarthur@gmail.com>
2026-07-15 14:59:37 +1200

    refactor(build): use MeshCore MC_VARIANT for board glue
    Compile xiao_nrf52 variant via MeshCore library mode like MCAppTemplate;
    SensorReader remains local and bypasses EnvironmentSensorManager at runtime.

--- pre_build.py
+++ pre_build.py
@@ -5,6 +5,14 @@ Import("env")  # type: ignore
 menv = env  # type: ignore
 
 env_name = menv["PIOENV"]
+variant_name = None
+
+for item in menv.get("BUILD_FLAGS", []):
+    if "MC_VARIANT" in item:
+        variant_name = item.split("=")[1]
+        variant_dir = f".pio/libdeps/{env_name}/MeshCore/variants/{variant_name}"
+        menv.Append(BUILD_FLAGS=[f"-I {variant_dir}"])
+
 libdeps = f".pio/libdeps/{env_name}/"
 mc_dir = libdeps + "MeshCore/"
 
